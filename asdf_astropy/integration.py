d54c0898fdeed4ecbf67377a782421de43bcd40b
Ed Slavich <eslavich@stsci.edu>
2020-09-04 08:46:39 -0400

    Add astropy-specific schemas to repo

---
+++ asdf_astropy/integration.py
@@ -0,0 +1,24 @@
+import sys
+
+from asdf.resource import DirectoryResourceMapping
+
+if sys.version_info < (3, 9):
+    import importlib_resources
+else:
+    import importlib.resources as importlib_resources
+
+from . import resources
+
+
+def get_resource_mappings():
+    """
+    Get the resource mapping instances for the astropy schemas
+    and manifests.  This method is registered with the
+    asdf.resource_mappings entry point.
+    """
+    resources_root = importlib_resources.files(resources)
+
+    return [
+        DirectoryResourceMapping(resources_root / "schemas", "http://astropy.org/schemas/astropy", recursive=True),
+        DirectoryResourceMapping(resources_root / "manifests", "http://astropy.org/asdf/extensions/astropy/manifests"),
+    ]
