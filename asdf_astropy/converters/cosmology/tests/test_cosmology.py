6001a943b8bec5a6f095f0fb7cf6bacfb4169dbf
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2021-11-08 23:52:33 -0500

    fix flrw ref
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- asdf_astropy/converters/cosmology/tests/test_cosmology.py
+++ asdf_astropy/converters/cosmology/tests/test_cosmology.py
@@ -11,6 +11,13 @@ from astropy.cosmology import parameters, realizations
 TEST_COSMOLOGIES = (getattr(realizations, n) for n in parameters.available)
 
 
+def assert_cosmo_equal(a, b):
+    __tracebackhide__ = True
+
+    assert a == b
+    assert a.meta == b.meta
+
+
 @pytest.mark.parametrize("cosmo", TEST_COSMOLOGIES)
 def test_serialization(cosmo, tmp_path):
     file_path = tmp_path / "test.asdf"
@@ -19,5 +26,4 @@ def test_serialization(cosmo, tmp_path):
         af.write_to(file_path)
 
     with asdf.open(file_path) as af:
-        assert af["cosmo"] == cosmo
-        assert af["cosmo"].meta == cosmo.meta
+        assert_cosmo_equal(af["cosmo"], cosmo)
