8402535f74d613311b3946d024680af75d1bcebc
William Jamieson <wjamieson@stsci.edu>
2022-05-31 12:55:51 -0400

    Add tests for all astropy time formats.

--- asdf_astropy/converters/time/tests/test_time.py
+++ asdf_astropy/converters/time/tests/test_time.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import asdf
 import numpy as np
 import pytest
@@ -10,18 +12,34 @@ from asdf_astropy.testing.helpers import assert_time_equal
 
 def create_times():
     return [
-        Time(np.arange(100), format="unix"),
+        Time(1950.0, format="byear"),
+        Time("B1950.0", format="byear_str"),
         Time(
             [1, 2],
             location=EarthLocation(x=[1, 2] * u.m, y=[3, 4] * u.m, z=[5, 6] * u.m),
             format="cxcsec",
         ),
+        Time(datetime(2000, 1, 2, 12, 0, 0), format="datetime"),
+        Time(2000.45, format="decimalyear"),
+        Time("2000-01-01T00:00:00.000", format="fits"),
+        Time(630720013.0, format="gps"),
+        Time("2000-01-01 00:00:00.000", format="iso"),
+        Time("2000-01-01T00:00:00.000", format="isot"),
+        Time(2451544.5, format="jd"),
+        Time(2000.0, format="jyear"),
         Time(
             "J2000.000",
             location=EarthLocation(x=6378100 * u.m, y=0 * u.m, z=0 * u.m),
             format="jyear_str",
         ),
+        Time(51544.0, format="mjd"),
+        # Time(730120.0003703703, format="plot_date"),
+        Time(np.arange(100), format="unix"),
+        Time(946684800.0, format="unix_tai"),
+        Time("2000:001:00:00:00.000", format="yday"),
         Time("2000-01-01T00:00:00.000"),
+        Time({"year": 2010, "month": 3, "day": 1}, format="ymdhms"),
+        Time(np.datetime64("2000-01-01T01:01:01"), format="datetime64"),
         Time(["2001-01-02T12:34:56", "2001-02-03T00:01:02"]),
     ]
 
