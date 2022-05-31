06fab2e0de51057d380fa568db407ef1748b6ca4
William Jamieson <wjamieson@stsci.edu>
2022-05-31 14:52:12 -0400

    Extend testing to more cases

--- asdf_astropy/converters/time/tests/test_time.py
+++ asdf_astropy/converters/time/tests/test_time.py
@@ -33,7 +33,7 @@ def create_times():
             format="jyear_str",
         ),
         Time(51544.0, format="mjd"),
-        # Time(730120.0003703703, format="plot_date"),
+        Time(730120.0003703703, format="plot_date"),
         Time(np.arange(100), format="unix"),
         Time(946684800.0, format="unix_tai"),
         Time("2000:001:00:00:00.000", format="yday"),
@@ -54,3 +54,59 @@ def test_serialization(time, version, tmp_path):
 
     with asdf.open(file_path) as af:
         assert_time_equal(af["time"], time)
+
+
+def create_examples():
+    return [
+        {"example": '!time/time-1.1.0 "2000-12-31T13:05:27.737"', "truth": Time("2000-12-31T13:05:27.737")},
+        {"example": '!time/time-1.1.0 "2001:003:04:05:06.789"', "truth": Time("2001:003:04:05:06.789")},
+        {"example": "!time/time-1.1.0 B2000.0", "truth": Time("B2000.0")},
+        {
+            "example": """!time/time-1.1.0
+          value: 2000.0
+          format: byear""",
+            "truth": Time(2000.0, format="byear"),
+        },
+        {
+            "example": """!time/time-1.1.0
+          ["2000-12-31T13:05:27.737", "2000-12-31T13:06:38.444"]""",
+            "truth": Time(["2000-12-31T13:05:27.737", "2000-12-31T13:06:38.444"]),
+        },
+        {
+            "example": """!time/time-1.1.0
+          value: !core/ndarray-1.0.0
+            data: [2000, 2001]
+            datatype: float64
+          format: jyear""",
+            "truth": Time([2000, 2001], format="jyear"),
+        },
+        {
+            "example": """!time/time-1.1.0
+          value: !core/ndarray-1.0.0
+          value: 2000.0
+          format: jyear
+          scale: tdb
+          location:
+            x: !unit/quantity-1.1.0
+              value: 6378100
+              unit: !unit/unit-1.0.0 m
+            y: !unit/quantity-1.1.0
+              value: 0
+              unit: !unit/unit-1.0.0 m
+            z: !unit/quantity-1.1.0
+              value: 0
+              unit: !unit/unit-1.0.0 m
+          format: jyear""",
+            "truth": Time(
+                2000.0, location=EarthLocation(x=6378100 * u.m, y=0 * u.m, z=0 * u.m), scale="tdb", format="jyear"
+            ),
+        },
+    ]
+
+
+@pytest.mark.parametrize("example", create_examples())
+def test_read_examples(example):
+    buff = asdf.tests.helpers.yaml_to_asdf(f"example: {example['example'].strip()}")
+    with asdf.AsdfFile() as af:
+        af._open_impl(af, buff, mode="rw")
+        assert np.all(af["example"] == example["truth"])
