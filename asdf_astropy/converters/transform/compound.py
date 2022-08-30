861cdcb20f9c5f70bf51bc1db911d3e3eabbfbcb
William Jamieson <wjamieson@stsci.edu>
2022-08-30 13:41:35 -0400

    Switch to f-string

--- asdf_astropy/converters/transform/compound.py
+++ asdf_astropy/converters/transform/compound.py
@@ -72,13 +72,13 @@ class CompoundConverter(TransformConverterBase):
 
         left = node["forward"][0]
         if not isinstance(left, Model):
-            raise TypeError("Unknown left model type '{}'".format(node["forward"][0]._tag))
+            raise TypeError(f"Unknown left model type '{node['forward'][0]._tag}'")
 
         right = node["forward"][1]
         if (oper == "fix_inputs" and not isinstance(right, dict)) or (
             oper != "fix_inputs" and not isinstance(right, Model)
         ):
-            raise TypeError("Unknown right model type '{}'".format(node["forward"][1]._tag))
+            raise TypeError(f"Unknown right model type '{node['forward'][1]._tag}'")
 
         if oper == "fix_inputs":
             right = dict(zip(right["keys"], right["values"]))
