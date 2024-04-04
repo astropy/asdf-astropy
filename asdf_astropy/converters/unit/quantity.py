4e764c1df5095f031de01388862fac7784f66d48
Brett <brettgraham@gmail.com>
2024-04-04 14:08:57 -0400

    update copy for numpy 2.0/astropy 6.1

--- asdf_astropy/converters/unit/quantity.py
+++ asdf_astropy/converters/unit/quantity.py
@@ -23,7 +23,13 @@ class QuantityConverter(Converter):
         return node
 
     def from_yaml_tree(self, node, tag, ctx):
+        # numpy 2.0 changed behavior for copy where an error is produced
+        # if False and a copy is required (previously there was no error)
+        # astropy 6.1 changed Quantity in a similar way
+        import numpy as np
         from astropy.units import Quantity
+
+        copy = None if np.lib.NumpyVersion(np.__version__) >= "2.0.0b1" else False
 
         value = node["value"]
         dtype = node.get("datatype", None)
@@ -33,4 +39,4 @@ class QuantityConverter(Converter):
             value = value._make_array()
             dtype = value.dtype
 
-        return Quantity(value, unit=node["unit"], copy=False, dtype=dtype)
+        return Quantity(value, unit=node["unit"], copy=copy, dtype=dtype)
