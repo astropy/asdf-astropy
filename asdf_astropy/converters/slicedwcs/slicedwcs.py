66904fc1de9617660a75c0f77c176b74930c6583
ViciousEagle03 <piyushsharma04321@gmail.com>
2024-08-24 04:56:30 +0530

    Apply suggestions from code review

--- asdf_astropy/converters/slicedwcs/slicedwcs.py
+++ asdf_astropy/converters/slicedwcs/slicedwcs.py
@@ -1,5 +1,3 @@
-import ast
-
 from asdf.extension import Converter
 
 
@@ -7,27 +5,30 @@ class SlicedWCSConverter(Converter):
     tags = ("tag:astropy.org:astropy/slicedwcs/slicedwcs-*",)
     types = ("astropy.wcs.wcsapi.wrappers.sliced_wcs.SlicedLowLevelWCS",)
 
-    def parse_slice_string(self, slice_str):
-        if slice_str.isdigit():
-            return int(ast.literal_eval(slice_str))
-        slice_str = slice_str[len("slice(") : -1]
-        parts = slice_str.split(",")
-        start = ast.literal_eval(parts[0].strip())
-        stop = ast.literal_eval(parts[1].strip())
-        step = ast.literal_eval(parts[2].strip())
-        return slice(start, stop, step)
-
     def from_yaml_tree(self, node, tag, ctx):
         from astropy.wcs.wcsapi.wrappers.sliced_wcs import SlicedLowLevelWCS
 
         wcs = node["wcs"]
-        slice_array = node["slices_array"]
-        slice_array = [self.parse_slice_string(s) for s in slice_array]
-
+        slice_array = []
+        slice_array = [
+            s if isinstance(s, int) else slice(s["start"], s["stop"], s["step"]) for s in node["slices_array"]
+        ]
         return SlicedLowLevelWCS(wcs, slice_array)
 
     def to_yaml_tree(self, sl, tag, ctx):
         node = {}
         node["wcs"] = sl._wcs
-        node["slices_array"] = [str(item) for item in sl._slices_array]
+        node["slices_array"] = []
+
+        for s in sl._slices_array:
+            if isinstance(s, slice):
+                node["slices_array"].append(
+                    {
+                        "start": s.start if s.start is not None else None,
+                        "stop": s.stop if s.stop is not None else None,
+                        "step": s.step if s.step is not None else None,
+                    },
+                )
+            else:
+                node["slices_array"].append(s)
         return node
