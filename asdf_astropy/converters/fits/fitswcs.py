5afd469317026e2dae8a928bc23d57f9723d9b41
ViciousEagle03 <piyushsharma04321@gmail.com>
2024-08-25 15:12:34 +0530

    Fix tests and schema

--- asdf_astropy/converters/fits/fitswcs.py
+++ asdf_astropy/converters/fits/fitswcs.py
@@ -8,10 +8,10 @@ class FitsWCSConverter(Converter):
     def from_yaml_tree(self, node, tag, ctx):
         from astropy.wcs import WCS
 
-        primary_hdu = node["Hdu"][0]
+        primary_hdu = node["hdu"][0]
         return WCS(primary_hdu.header)
 
     def to_yaml_tree(self, wcs, tag, ctx):
         node = {}
-        node["Hdu"] = wcs.to_fits()
+        node["hdu"] = wcs.to_fits()
         return node
--- asdf_astropy/converters/fits/tests/test_fitswcs.py
+++ asdf_astropy/converters/fits/tests/test_fitswcs.py
@@ -18,16 +18,14 @@ def create_wcs():
         return [WCS(fits.open(download_file(url, cache=True))[0].header) for url in urls]
 
 
+@pytest.mark.filterwarnings("ignore::astropy.wcs.wcs.FITSFixedWarning")
 @pytest.mark.parametrize("wcs", create_wcs())
 def test_wcs_serialization(wcs, tmp_path):
     file_path = tmp_path / "test_wcs.asdf"
-    with asdf.AsdfFile() as af, warnings.catch_warnings():
-        warnings.simplefilter("ignore", FITSFixedWarning)
+    with asdf.AsdfFile() as af:
         af["wcs"] = wcs
         af.write_to(file_path)
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", FITSFixedWarning)
-        with asdf.open(file_path) as af:
-            loaded_wcs = af["wcs"]
-            assert wcs.to_header() == loaded_wcs.to_header()
+    with asdf.open(file_path) as af:
+        loaded_wcs = af["wcs"]
+        assert wcs.to_header() == loaded_wcs.to_header()
