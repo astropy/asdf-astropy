03b80b67083a0705bd7f7644e3f71fbad2d2ab46
Leo Singer <leo.singer@ligo.org>
2025-03-25 18:49:42 -0400

    Fix test

--- asdf_astropy/converters/tests/test_utils.py
+++ asdf_astropy/converters/tests/test_utils.py
@@ -3,10 +3,7 @@ import pytest
 from asdf_astropy.converters.utils import import_masked_type
 from asdf_astropy.tests.versions import ASTROPY_LT_7_1
 
-if not ASTROPY_LT_7_1:
-    pytest.skip(reason="MaskedQuantity support was added in astropy 7.1", allow_module_level=True)
-
 
 def test_import_masked_type():
-    with pytest.raises(AttributeError):
+    with pytest.raises(NotImplementedError if ASTROPY_LT_7_1 else AttributeError):
         import_masked_type("ThisClassDoesNotExist")
