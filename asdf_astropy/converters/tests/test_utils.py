45645cd86d5ac0b0f0453125c6be0dd77b759280
Leo Singer <leo.singer@ligo.org>
2025-03-25 18:44:14 -0400

    Fix test coverage

--- asdf_astropy/converters/tests/test_utils.py
+++ asdf_astropy/converters/tests/test_utils.py
@@ -3,7 +3,7 @@ import pytest
 from asdf_astropy.converters.utils import import_masked_type
 from asdf_astropy.tests.versions import ASTROPY_LT_7_1
 
-if ASTROPY_LT_7_1:
+if not ASTROPY_LT_7_1:
     pytest.skip(reason="MaskedQuantity support was added in astropy 7.1", allow_module_level=True)
 
 
