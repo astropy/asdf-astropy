187d983bf4b513472b00a809b01fc2d8ab515032
Leo Singer <leo.singer@ligo.org>
2025-03-25 16:01:41 -0400

    Fix test coverage

---
+++ asdf_astropy/converters/tests/test_utils.py
@@ -0,0 +1,10 @@
+import pytest
+
+from asdf_astropy.converters.utils import import_masked_type
+from asdf_astropy.testing.helpers import skip_if_astropy_lt_7_1
+
+
+@skip_if_astropy_lt_7_1
+def test_import_masked_type():
+    with pytest.raises(AttributeError):
+        import_masked_type("ThisClassDoesNotExist")
