9d6c76f3bd4f4ccb7cbf289283e6990535abdf98
Leo Singer <leo.p.singer@nasa.gov>
2025-03-26 09:52:17 -0400

    Update asdf_astropy/converters/unit/tests/test_masked.py
    Co-authored-by: Brett Graham <brettgraham@gmail.com>

--- asdf_astropy/converters/unit/tests/test_masked.py
+++ asdf_astropy/converters/unit/tests/test_masked.py
@@ -25,8 +25,6 @@ def test_masked_quantity(tmp_path):
     with asdf.open(file_path) as af:
         assert isinstance(af["quantity"], MaskedQuantity)
         assert af["quantity"].unit == u.yottamole
-        # FIXME: ASTROPY_LT_7_1: move import to module scope once we depend on astropy >= 7.1
-        from astropy.utils.masked import get_data_and_mask
 
         result_data, result_mask = get_data_and_mask(af["quantity"].value)
         np.testing.assert_array_equal(result_data, data)
