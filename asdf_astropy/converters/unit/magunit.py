955fac7473e932546bced050584cd2b91b8c0967
Brett <brettgraham@gmail.com>
2026-02-12 08:09:21 -0500

    fix magunit serialization to match unit

--- asdf_astropy/converters/unit/magunit.py
+++ asdf_astropy/converters/unit/magunit.py
@@ -1,14 +1,31 @@
-from asdf.extension import Converter
+from asdf.versioning import split_tag_version
+
+from .unit import UnitConverter
 
 
-class MagUnitConverter(Converter):
+class MagUnitConverter(UnitConverter):
     tags = ("tag:astropy.org:astropy/units/magunit-*",)
     types = ("astropy.units.function.logarithmic.MagUnit",)
 
+    def select_tag(self, obj, tags, ctx):
+        return tags[-1]
+
     def to_yaml_tree(self, obj, tag, ctx):
-        return {"unit": obj.physical_unit}
+        _, version = split_tag_version(tag)
+
+        # version 1.0.0 uses a nested dictionary
+        if version == "1.0.0":
+            return {"unit": obj.physical_unit}
+
+        return super().to_yaml_tree(obj.physical_unit, tag, ctx)
 
     def from_yaml_tree(self, node, tag, ctx):
         from astropy.units import mag
 
-        return mag(node["unit"])
+        _, version = split_tag_version(tag)
+
+        # version 1.0.0 uses a nested dictionary
+        if version == "1.0.0":
+            return mag(node["unit"])
+
+        return mag(super().from_yaml_tree(node, tag, ctx))
