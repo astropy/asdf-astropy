74041dd952d9a5d70bc5e5dfd3272b49627461be
Brett <brettgraham@gmail.com>
2026-08-13 11:03:32 -0400

    remove checks for out-dated asdf-coordinates-schemas

--- asdf_astropy/extensions.py
+++ asdf_astropy/extensions.py
@@ -537,15 +537,11 @@ ASTROPY_CONVERTERS = [
 
 
 _COORDINATES_MANIFEST_URIS = [
+    "asdf://asdf-format.org/astronomy/coordinates/manifests/coordinates-1.3.0",
     "asdf://asdf-format.org/astronomy/coordinates/manifests/coordinates-1.2.0",
     "asdf://asdf-format.org/astronomy/coordinates/manifests/coordinates-1.1.0",
     "asdf://asdf-format.org/astronomy/coordinates/manifests/coordinates-1.0.0",
 ]
-
-
-_asdf_coordinates_schemas_version = importlib.metadata.version("asdf-coordinates-schemas")
-if _asdf_coordinates_schemas_version >= "0.5.0":
-    _COORDINATES_MANIFEST_URIS.insert(0, "asdf://asdf-format.org/astronomy/coordinates/manifests/coordinates-1.3.0")
 
 
 COORDINATES_EXTENSIONS = [
